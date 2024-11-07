b4a0b775a8197c8b213eef11c8b5a75fd8c4d9b6
Netanel Rotenstein <email@example.com>
2024-11-07 11:20:36 +0200

    3.4 complete

--- src/utils/BuildingDataProvider.ts
+++ src/utils/BuildingDataProvider.ts
@@ -13,23 +13,38 @@ const useBuildingData = () => {
   const [buildingData, setBuildingData] = useState<Floor[]>([]);
 
   //FILL HERE LOGIC TO SET THE BUILDING DATA
-
-  useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = async ():Promise<Floor[]> => {
       const floorString: string = await fs.readFile(`./data/building.json`, `utf-8`)
-      const finalData = await JSON.parse(floorString)
+      const finalData:Floor[] = await JSON.parse(floorString)
       await setBuildingData(finalData)
-    }, []
+      return finalData
+    }
   fetchData();
-  });
-  return buildingData;
-}
-const getFloorByIndex = (floorIndex: number): Floor | undefined => {
+
+const getFloorByIndex = async (floorIndex: number): Promise<Floor | undefined> => {
   //FILL HERE
-  const allData:Floor[] = useBuildingData()
+  const allData:Floor[] = await fetchData()
+  switch (floorIndex) {
+    case 1:
+      return allData.find(f => f.name == "First Floor")
+    case 2:
+      return allData.find(f => f.name == "Second Floor")
+    case 3:
+      return allData.find(f => f.name == "Third Floor")
+    case 4:
+      return allData.find(f => f.name == "Fourth Floor")
+    case 5:
+      return allData.find(f => f.name == "Fifth Floor")
+
+    default:
+      break;
+  }
+
 }
-const getListOfActivities = (): string[] => {
+const getListOfActivities = async (): Promise<string[]> => {
   //FILL HERE
+  const allData: Floor[] = await fetchData()
+  return allData.map(a => a.activity)
 }
 return {
   buildingData,
@@ -37,5 +52,6 @@ return {
   getListOfActivities
 };
 };
+
 
 export default useBuildingData;
